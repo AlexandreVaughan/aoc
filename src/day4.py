dfd79ce60d8ba00dc9ca9371d5fc55841e4dd256
Alexandre Vaughan <49861406+avaughan468@users.noreply.github.com>
2023-12-04 22:03:27 +0100

    aoc 2023 day 4 part 2

--- src/day4.py
+++ src/day4.py
@@ -2,20 +2,27 @@ class Card:
     def __init__(self) -> None:
         self.winning_numbers = set()
         self.card_numbers = set()
+        self.id = 0
     
     def win_value(self):
-        win_val = 0
+        win_count = self.win_count()
+        if win_count == 0:
+            return 0
+        return 2**(self.win_count()-1)
+    
+    def win_count(self):
+        win_count = 0
         for num in self.card_numbers:
             if not num in self.winning_numbers:
                 continue
-            if win_val == 0:
-                win_val = 1
-            else:
-                win_val *= 2
-        return win_val
+            win_count+=1
+        return win_count
+
 
     def set_definition(self,definition : str):
-        (_,_,card_content) = definition.partition(":")
+        (card_id,_,card_content) = definition.partition(":")
+        (_,_,id) = card_id.partition(" ")
+        self.id = int(id)
         (winning_numbers_str,_,card_numbers_str) = card_content.partition("|")
         self.winning_numbers = _read_num_list(winning_numbers_str)
         self.card_numbers = _read_num_list(card_numbers_str)
@@ -28,8 +35,39 @@ def _read_num_list(numbers_list_str):
         numbers_list.add(int(num))
     return numbers_list
 
+class CardList:
+    def __init__(self) -> None:
+        self.cards = []
+
+    def set_definition(self, card_list_def: str):
+        for card_def in card_list_def.splitlines():
+            card = Card()
+            card.set_definition(card_def)
+            self.cards.append(card)
+    
+    def copy_count(self, card_id, recursive):
+        if card_id < 1 or card_id >= len(self.cards):
+            return 0
+        card : Card = self.cards[card_id-1]
+        if not recursive:
+            return card.win_count()
+        copies_count = card.win_count()
+        if copies_count == 0:
+            return copies_count
+        for copy_id in range(card_id+1,card_id+1+copies_count):
+            copies_count += self.copy_count(copy_id,recursive)
+        return copies_count
+    
+    def total_won_card(self):
+        total_count = len(self.cards)
+        for card in self.cards:
+            total_count += self.copy_count(card.id,True)
+        return total_count
+
+
+
 if __name__ == "__main__":
-    f = open("D:\\Sources\\sample_projects\\aoc2023\\tests\\day4.txt", "r")
+    f = open("/media/alexandre/DATA/Sources/aoc2023/tests/day4.txt", "r")
     txt = f.read()
     sum = 0
     for line in txt.splitlines():
@@ -37,5 +75,8 @@ if __name__ == "__main__":
         card.set_definition(line)
         sum+=card.win_value()
     print(sum)
+    card_list = CardList()
+    card_list.set_definition(txt)
+    print(card_list.total_won_card())
 
     
--- tests/test_day4.py
+++ tests/test_day4.py
@@ -1,37 +1,72 @@
-from day4 import Card
+from day4 import Card, CardList
 
 def test_nowin():
     card = Card()
     card.winning_numbers = {12,13,14}
     card.card_numbers = {20,15,16}
     assert card.win_value() == 0
+    assert card.win_count() == 0
 
 def test_onewin():
     card = Card()
     card.winning_numbers = {12,13,14}
     card.card_numbers = {20,14,16}
     assert card.win_value() == 1
+    assert card.win_count() == 1
 
 def test_twowins():
     card = Card()
     card.winning_numbers = {12,13,14}
     card.card_numbers = {20,14,13}
     assert card.win_value() == 2
+    assert card.win_count() == 2
 
 def test_threewins():
     card = Card()
     card.winning_numbers = {12,13,14}
     card.card_numbers = {12,14,13}
     assert card.win_value() == 4
+    assert card.win_count() == 3
 
 def test_fourwins():
     card = Card()
     card.winning_numbers = {41,48,83,86,17}
     card.card_numbers = {83,86,6,31,17,9,48,53}
     assert card.win_value() == 8
+    assert card.win_count() == 4
 
 def test_card_definition():
     card = Card()
     card.set_definition("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
     assert card.winning_numbers == {41,48,83,86,17}
     assert card.card_numbers == {83,86,6,31,17,9,48,53}
+    assert card.id == 1
+
+CARD_LIST_DEF = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
+def test_card_list():
+    card_list = CardList()
+    card_list.set_definition(CARD_LIST_DEF)
+    assert len(card_list.cards) == 6
+    assert card_list.cards[2].id == 3
+
+def test_card_list_copy_1card_1level():
+    card_list = CardList()
+    card_list.set_definition(CARD_LIST_DEF)
+    assert card_list.copy_count(1,False) == 4
+
+def test_card_list_copy_1card_recursive():
+    card_list = CardList()
+    card_list.set_definition(CARD_LIST_DEF)
+    assert card_list.copy_count(1,True) == 14
+
+def test_card_list_total_won_cards():
+    card_list = CardList()
+    card_list.set_definition(CARD_LIST_DEF)
+    assert card_list.total_won_card() == 30
+
