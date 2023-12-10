79f73c9ed9c4f60d4c47433ce7129cb65b76adc3
Alexandre Vaughan <alexandre.vaughan@hexagon.com>
2023-12-10 09:12:58 +0100

    aoc2023 day 10 part 1

---
+++ src/day10.py
@@ -0,0 +1,124 @@
+from queue import Queue
+
+NORTH = 0
+SOUTH = 1
+EAST = 2
+WEST = 3
+
+class Pipe:
+    def __init__(self,definition: str) -> None:
+        self.allowed_directions = _allowed_directions(definition)
+        self.distance = -1
+
+class PipeGrid:
+    def __init__(self, definition:str) -> None:
+        self.grid = []
+        self.starting_point = (0,0)
+        self._read_grid(definition)
+        self._find_startingpoint_directions()
+
+    def _read_grid(self,definition:str):
+        line_idx=0
+        for line in definition.splitlines():
+            grid_line = []
+            column_idx =0
+            for cell in line:
+                grid_line.append(Pipe(cell))
+                if cell == "S":
+                    self.starting_point = (column_idx,line_idx)
+                column_idx+=1
+            self.grid.append(grid_line)
+            line_idx +=1
+
+    def _find_startingpoint_directions(self):
+        final_directions = []
+        (xstart,ystart) = self.starting_point
+        for direction in self.directions(xstart,ystart):
+            new_point = _move(xstart,ystart,direction)
+            if not _opposite_direction(direction) in self.directions(*new_point):
+                continue
+            final_directions.append(direction)
+        self.grid[ystart][xstart].allowed_directions = final_directions
+        
+    def directions(self,x,y):
+        pipe = self._pipe_at(x,y)
+        if pipe is None:
+            return []
+        return pipe.allowed_directions
+    
+    def distance(self,x,y):
+        pipe = self._pipe_at(x,y)
+        if pipe is None:
+            return -1
+        return pipe.distance
+    
+    def _pipe_at(self,x,y):
+        if x < 0 or y < 0:
+            return None
+        if x >= len(self.grid[0]) or y >= len(self.grid):
+            return None
+        return self.grid[y][x]
+    
+    def find_distances(self):
+        queue = Queue()
+        queue.put((*self.starting_point,0))
+        while not queue.empty():
+            (x,y,distance) = queue.get()
+            pipe = self._pipe_at(x,y)
+            if pipe is None:
+                continue
+            if pipe.distance != -1:
+                continue
+            pipe.distance = distance
+            for direction in self.directions(x,y):
+                queue.put((*(_move(x,y,direction)),distance+1))
+
+        
+    def max_distance(self):
+        max_dist = 0
+        for line in self.grid:
+            for pipe in line:
+                max_dist = max(max_dist,pipe.distance)
+        return max_dist
+
+
+    
+
+
+def _allowed_directions(pipe: str):
+    DIRECTION_MAP = {
+        ".": [],
+        "|": [NORTH,SOUTH],
+        "-": [EAST,WEST],
+        "J": [NORTH,WEST],
+        "7": [SOUTH,WEST],
+        "L": [NORTH,EAST],
+        "F": [SOUTH,EAST],
+        "S": [NORTH,SOUTH,EAST,WEST],
+    }
+    return DIRECTION_MAP[pipe]
+
+def _move(x,y,direction):
+    move_map = {
+        NORTH: (x,y-1),
+        SOUTH: (x,y+1),
+        WEST: (x-1,y),
+        EAST: (x+1,y)
+    }
+    return move_map[direction]
+
+def _opposite_direction(direction):
+    direction_map = {
+        NORTH: SOUTH,
+        SOUTH: NORTH,
+        WEST: EAST,
+        EAST: WEST
+    } 
+    return direction_map[direction] 
+
+if __name__ == "__main__":
+    f = open("D:\\Sources\\sample_projects\\aoc2023\\tests\\day10.txt", "r")
+    txt = f.read()
+    pipe_grid = PipeGrid(txt)
+    pipe_grid.find_distances()
+    print(pipe_grid.max_distance())
---
+++ tests/test_day10.py
@@ -0,0 +1,63 @@
+from day10 import *
+
+SIMPLE_LOOP = """.....
+.S-7.
+.|.|.
+.L-J.
+....."""
+
+OTHER_LOOP="""-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF"""
+
+COMPLEX_LOOP="""7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ"""
+
+def test_allowed_directions():
+    assert Pipe(".").allowed_directions == []
+    assert Pipe("|").allowed_directions == [NORTH,SOUTH]
+    assert Pipe("-").allowed_directions == [EAST,WEST]
+    assert Pipe("J").allowed_directions == [NORTH,WEST]
+    assert Pipe("7").allowed_directions == [SOUTH,WEST]
+    assert Pipe("L").allowed_directions == [NORTH,EAST]
+    assert Pipe("F").allowed_directions == [SOUTH,EAST]
+    assert Pipe("S").allowed_directions == [NORTH,SOUTH,EAST,WEST]
+
+def test_pipe_grid():
+    pipe_grid = PipeGrid(SIMPLE_LOOP)
+    assert pipe_grid.directions(0,0) == []
+    assert pipe_grid.starting_point == (1,1)
+    assert pipe_grid.directions(3,1) == [SOUTH,WEST]
+    assert pipe_grid.directions(-1,1) == []
+    assert pipe_grid.directions(1,90) == []
+    
+def test_starting_point_directions():
+    pipe_grid = PipeGrid(SIMPLE_LOOP)
+    assert pipe_grid.directions(*pipe_grid.starting_point) == [SOUTH,EAST]
+
+    pipe_grid = PipeGrid(OTHER_LOOP)
+    assert pipe_grid.directions(*pipe_grid.starting_point) == [SOUTH,EAST]
+
+def test_distance_simple_grid():
+    pipe_grid = PipeGrid(SIMPLE_LOOP)
+    pipe_grid.find_distances()
+    assert pipe_grid.distance(*pipe_grid.starting_point) == 0
+    assert pipe_grid.distance(2,1) == 1
+    assert pipe_grid.distance(3,1) == 2
+    assert pipe_grid.distance(3,3) == 4
+
+def test_max_distance():
+    pipe_grid = PipeGrid(SIMPLE_LOOP)
+    pipe_grid.find_distances()
+    assert pipe_grid.max_distance() == 4
+    pipe_grid = PipeGrid(OTHER_LOOP)
+    pipe_grid.find_distances()
+    assert pipe_grid.max_distance() == 4
+    pipe_grid = PipeGrid(COMPLEX_LOOP)
+    pipe_grid.find_distances()
+    assert pipe_grid.max_distance() == 8
